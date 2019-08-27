dc434549090c2b359aa4e35bc624aafc61735ba4
P-O Quirion <pioliqui@gmail.com>
2019-08-27 16:20:29 -0400

    split chr

--- gcta64_gnu_paralle_wrapper.sh
+++ gcta64_gnu_paralle_wrapper.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+export GCTA_BASEDIR=${GCT_BASEDIR=/lustre03/project/6004777/projects/uk_biobank/GCTA/GTEx2_data_june_2019/lustre03/project/6004777/projects/uk_biobank/GCTA/GTEx2_data_june_2019/}
+
+
+export GCTA_FULL_UREL_DIR=${GCT_FULL_UREL_DIR=/lustre03/project/6004777/projects/uk_biobank/GCTA/full_unrel/}
 
 
 usage () {
 
-cat  << EO_MARCUS_MAIL 
-  $0 <tissue> <chr> <ma_file> <outname> -f <filterd_unrelated_path>  -b <basepath> 
+cat  << EO_MARCUS_MAIL
+  $0  <tissue> <chr> <ma_file> <outname>
+  tissue = Tissue name
+  chr = chromosome
+  ma_file = table with conditioning variants
+  outname = name for output file
 
-
-\${tissue} = Tissue name (can be used in output dir)
-\${chr} = chromosome = --bfile for gcta (add path/filename: "/lustre03/project/6004777/projects/uk_biobank/GCTA/full_unrel/filtered_unrelated_chr"
-\${ma_file} = table with conditioning variants
-\${outname} = name for output file
-/lustre03/project/6004777/projects/uk_biobank/GCTA/full_unrel/filtered_unrelated_chr\${chr} = full named path and --bfile (redundant to $chr)
-\${BASEPATH}/\${tissue}/snplist_files/snplist_\${ma_file} = --extract file
-\${BASEPATH}/\${tissue}/\${ma_file} = --cojo-file, simple list
-\${BASEPATH}/\${tissue}/out/\${outname} = output name incl directory for my downstream R runs.
+  GCTA_BASEDIR  = ${GCT_BASEDIR}
+  GCTA_FULL_UREL_DIR = ${GCTA_FULL_UREL_DIR}
+  you can set these two env vars to change their value
 EO_MARCUS_MAIL
 }
 
@@ -35,10 +37,6 @@ outname=$4
 
 GCTA_BIN=~/bin/gcta/gcta_1.92.0beta6/gcta64
 
-GCTA_BASEDIR=${GCT_BASEDIR=/lustre03/project/6004777/projects/uk_biobank/GCTA/GTEx2_data_june_2019/lustre03/project/6004777/projects/uk_biobank/GCTA/GTEx2_data_june_2019/}
-
-
-export GCTA_FULL_UREL_DIR=${GCT_FULL_UREL_DIR=/lustre03/project/6004777/projects/uk_biobank/GCTA/full_unrel/}
 
 move_locally () {
   src_dir=$1
@@ -48,15 +46,15 @@ move_locally () {
   mkdir ${tmp_dir} 2>/dev/null
   not_first=$?
 
-  # if not first wait for file to be there 
+  # if not first wait for file to be there
   # if first transfert the file in .tmp and rename them when all is done.
-  if [ $not_first -eq 1 ] ; then 
+  if [ $not_first -eq 1 ] ; then
     echo transfer by other process
-    # wait for another process to finish the transfer 
+    # wait for another process to finish the transfer
     while [ ! -f ${tmp_dir}/${file_name}.fam ];   do
       sleep 2
     done
-  else 
+  else
     echo This process do the file transfere
     # you'r the first process, do the transfer yourself!
     mkdir ${tmp_dir}/.tmp
@@ -65,7 +63,7 @@ move_locally () {
     rm -r ${tmp_dir}/.tmp
 
   fi
- 
+
   echo transfere of ${file_name} to ${tmp_dir} done
 
 }
@@ -77,20 +75,22 @@ tmp_dir=${SLURM_TMPDIR}/${tmp_dir}_${file_name} 2>/dev/null
 
 move_locally ${GCTA_FULL_UREL_DIR}  $tmp_dir $file_input
 
+running
 cat << TRUITE
-${GCTA_BIN} \ 
+${GCTA_BIN} \
   --bfile ${tmp_dir}/${file_input} \
   --extract ${GCTA_BASEDIR}/${tissue}/snplist_files/snplist_${ma_file} \
   --thread $SLURM_CPUS_PER_TASK \
   --maf 0.01 \
   --cojo-file ${GCTA_BASEDIR}/${tissue}/${ma_file} \
-  --cojo-slct 
+  --cojo-slct
 TRUITE
 
-tree ${tmp_dir}
-
-
-
-
-
+${GCTA_BIN} \
+   --bfile ${tmp_dir}/${file_input} \
+  --extract ${GCTA_BASEDIR}/${tissue}/snplist_files/snplist_${ma_file} \
+  --thread $SLURM_CPUS_PER_TASK \
+  --maf 0.01 \
+  --cojo-file ${GCTA_BASEDIR}/${tissue}/${ma_file} \
+  --cojo-slct
 
